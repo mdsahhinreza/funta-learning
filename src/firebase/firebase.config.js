e120a1262ac75283bbe23d7e2f87b042076fafc7
mdsahhinreza <mdsahhinreza@gmail.com>
2022-10-25 22:31:23 +0600

    Project setup and firebase implement

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLWV_3jkG4vHBHYmI52CU9bTIziQOHZ3I",
+  authDomain: "funta-learning.firebaseapp.com",
+  projectId: "funta-learning",
+  storageBucket: "funta-learning.appspot.com",
+  messagingSenderId: "475702395806",
+  appId: "1:475702395806:web:12a27160f265c2a53b4c8f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
