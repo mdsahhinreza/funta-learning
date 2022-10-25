1b4a68054f6b32860dae9e84959611096a05bea9
mdsahhinreza <mdsahhinreza@gmail.com>
2022-10-26 00:05:57 +0600

    Context Added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h3>This Is footer</h3>
+    <div className="funta-bg text-light text-center pt-3 pb-2">
+      <p>Funta-Learning &copy; 2022-2100</p>
     </div>
   );
 };
